fc2c7ea246dc3262f7d5b0ff088086f087776d1c
zhangjinxing <zhangjx@minstone.com.cn>
2018-12-20 17:28:16 +0800

    重新修复

--- node/koa/koa.js
+++ node/koa/koa.js
@@ -1,28 +1,28 @@
 const Koa = require('koa');
 const app = new Koa();
-const koaBody = require('koa-body');
-const bodyParser = require('koa-bodyparser')
 const path = require('path');
 const static = require('koa-static');
 const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
+app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
-router.all('*',function (ctx, next) {
-    ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.set('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
-    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    ctx.set('X-Powered-By', '3.2.1')
-    next();
-});
 
-  
+
+
 
 
 // 设置静态路径
 const staticPath = './';
-app.use(bodyParser());
 app.use(static(path.join(__dirname, staticPath)));
 
+router.all('/test', (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    ctx.set('X-Powered-By', '3.2.1');
+    next(); 
+});
 router.post('/test', (ctx) => {
     console.log(ctx.request.body);
     ctx.body = ctx.request.body;
